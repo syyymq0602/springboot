d599597ae4af408e939ba89e3c4d63432bff45b8
tpl <tpl_vis_group@outlook.com>
2021-10-27 21:20:37 +0800

    实现了简单的登录注销功能，还未使用数据库数据进行登录验证

--- demo/src/main/java/swjtu/syyymq/config/SecurityConfig.java
+++ demo/src/main/java/swjtu/syyymq/config/SecurityConfig.java
@@ -1,29 +1,49 @@
 package swjtu.syyymq.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import swjtu.syyymq.entity.User;
+import swjtu.syyymq.service.UserService;
+
+import java.util.List;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserService userService;
     // 链式编程
     // 授权
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .loginProcessingUrl("/login");
 
-        super.configure(http);
+        http.csrf().disable();
+
+        http.logout().logoutSuccessUrl("/");
     }
 
     // 认证
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        List<User> users = userService.findAll();
+        if (users==null||users.size()==0){
+            throw new UsernameNotFoundException("用户未找到");
+        }
         auth.inMemoryAuthentication()
                 .passwordEncoder(new BCryptPasswordEncoder())
                 .withUser("admin").password(new BCryptPasswordEncoder().encode("123")).roles("")
--- demo/src/main/java/swjtu/syyymq/controller/LoginController.java
+++ demo/src/main/java/swjtu/syyymq/controller/LoginController.java
@@ -1,16 +1,14 @@
 package swjtu.syyymq.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-//@RequestMapping("/user")
+@RequestMapping("/user")
 public class LoginController {
 
-    @GetMapping("/login")
-    @ResponseBody
+    @PostMapping("/login")
     public String login(){
-        return "index";
+        return "dashboard";
     }
 }

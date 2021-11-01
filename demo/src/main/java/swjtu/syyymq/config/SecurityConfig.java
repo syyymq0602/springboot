d63b54fe7a699a6ceec9cb8981fe3d1fb4590a36
tpl <tpl_vis_group@outlook.com>
2021-11-01 14:57:58 +0800

    实现了登陆界面的国际化，修复了遗留的BUG，待做忘记密码以及注册替换

--- demo/src/main/java/swjtu/syyymq/config/SecurityConfig.java
+++ demo/src/main/java/swjtu/syyymq/config/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 // 放行主界面、登录界面、注册页面、邮箱验证请求
-                .antMatchers("/","/toLogin","/register","/sendEmail").permitAll()
+                .antMatchers("/","/toLogin","/register","/sendEmail","/login**").permitAll()
                 // 放行静态资源
                 .antMatchers("/css/**","/js/**","/images/**","/assets/**").permitAll()
                 // 根据授权访问对应资源
@@ -58,4 +58,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
+
 }
--- demo/src/main/java/swjtu/syyymq/config/i18nConfig.java
+++ demo/src/main/java/swjtu/syyymq/config/i18nConfig.java
@@ -3,11 +3,17 @@ package swjtu.syyymq.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import swjtu.syyymq.utils.CustomLocaleResolver;
 
 @Configuration
 public class i18nConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+    }
 
     @Bean
     public LocaleResolver localeResolver(){
